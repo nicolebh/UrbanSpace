89fd75caef3c1927e28e32db10c16123ad6bec31
nicolebh <nicole.marcus1@gmail.com>
2019-08-22 01:54:32 +0300

    login and signup front-end

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <html>
 <nav class="navbar navbar-expand-lg navbar-light space-navbar" style="background-color:white;">
             <a class="navbar-brand" href="#">
-                <img src="/include/logo.png" width="110px" height="70px">
+                <img src="http://nimrodba.mtacloud.co.il/include/logo.png" width="110px" height="70px">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -11,7 +11,7 @@
                     <a class="nav-item nav-link active" href="http://nimrodba.mtacloud.co.il">Home <span class="sr-only">(current)</span></a>
                     <a class="nav-item nav-link" href="http://nimrodba.mtacloud.co.il/#about-block">About</a>
                     <a class="nav-item nav-link" href="http://nimrodba.mtacloud.co.il/#feature-section">Features</a>
-                    <a class="nav-item nav-link" href=#>Login</a>
+                    <a class="nav-item nav-link" href=login.html>Login</a>
                 </div>
             </div>
     </nav>
